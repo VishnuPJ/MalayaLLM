a310ffb87ebb4a786e78eb48e96f8829d33fccb5
VishnuPJ <54801493+VishnuPJ@users.noreply.github.com>
2024-01-30 16:23:06 +0530

    Update download_base_llama.py

--- Pretrain/download_base_llama.py
+++ Pretrain/download_base_llama.py
@@ -1,4 +1,4 @@
-from hugginface_hub import snapshot_download
+from huggingface_hub import snapshot_download
 
 model_id = "meta-llama/Llama-2-7b-hf"
 snapshot_download(repo_id = model_id , local_dir = r"/llama_7b_hf",local_dir_use_symlinks = False,revision = "main")
